d38495ed7a981fa6fc6a7142e8a2106c212c31fc
Timi <tkutos2002@gmail.com>
2024-11-26 11:18:53 +0200

    routes + auth

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
 
+import cors from 'cors';
 import express from 'express';
 import session from 'express-session';
 import { creates } from './db/create.js';
 import loginRouter from './routes/loginRouter.js';
 import logoutRouter from './routes/logoutRouter.js';
-// import userRouter from './routes/userRouter.js';
+import userRouter from './routes/userRouter.js';
 // import questionRouter from './routes/questionRouter.js';
-// import categoryRouter from './routes/categoryRouter.js';
+import categoryRouter from './routes/categoryRouter.js';
 // import answerRouter from './routes/answerRouter.js';
 import authMiddleware from './middlewares/auth.js';
 
 creates();
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true, 
+  }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
         secret: '142e6ecf42884f03',
-        resave: true,
-        saveUninitialized: true,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { 
+            secure: false,              
+            maxAge: 3600000          
+          }
     }),
 );
 app.get('/', (req, res) => {
@@ -26,9 +35,9 @@ app.get('/', (req, res) => {
 });
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
-// app.use('/users', userRouter);
+app.use('/users', userRouter);
 // app.use('/questions', questionRouter);
-// app.use('/categories', categoryRouter);
+app.use('/categories', categoryRouter);
 // app.use('/answers', answerRouter);
 
 app.use(authMiddleware);
--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,23 +1,25 @@
-import express from 'express';
 import bcrypt from 'bcrypt';
+import express from 'express';
 import { insertSession } from '../db/queries/sessionQueries.js';
-import authorize from '../middlewares/auth.js';
-import { getUser, getUserPassword } from '../db/queries/userQueries.js';
+import { getUser } from '../db/queries/userQueries.js';
 
 const router = express.Router();
 router.use(express.json());
 
-router.post('/', authorize(), async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     let response = '';
     let success = true;
     const { username, password } = req.body;
-    const userId = await getUser(username);
-    if (userId === -1) {
+    const user = await getUser(username);
+    const userId = user.id;
+    console.log(userId);
+    if (userId === undefined) {
       response = 'User does not exist.';
       success = false;
     } else {
-      const userPassword = await getUserPassword(userId);
+      const userPassword = user.password;
+      console.log(userPassword);
       if (userPassword === -1) {
         response = 'Could not check password.';
         success = false;
@@ -27,10 +29,14 @@ router.post('/', authorize(), async (req, res) => {
       }
     }
     if (success) {
-      req.session.id = req.id;
+      //req.session.id = req.id;
       req.session.userId = userId;
       insertSession(userId);
-      res.redirect('/questions');
+      res.status(200).cookie('userId', userId, {
+      httpOnly: true, 
+      secure: false,  
+      maxAge: 3600000
+      }).send();
     }
   } catch (err) {
     console.log(err);
